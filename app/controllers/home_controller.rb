594de6a23a1f30e67cc2e77bbc0ce2ef007f1afd
Naiyyar Abbas <naiyyarabbas512013@gmail.com>
2016-08-25 13:49:47 +0530

    fix for typeahead search results

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,33 +3,33 @@ class HomeController < ApplicationController
   end
 
   def search
-  	#debugger
-  	#if params[:term].blank?
-  		@buildings = Building.text_search(params[:term])
-  	#else
-  		# second search with building name
-	  	@autosearch_buildings = Building.where('building_name @@ :q', q: params[:term])
-	    if @autosearch_buildings.present?
-	      @result_type = 'buildings'
-	    else
-	      # second search with city names
-	      @autosearch_buildings = Building.where('city @@ :q', q: params[:term])
-	      if @autosearch_buildings.present?
-	        @result_type = 'cities'
-	      else
-	        # in last search with building address
-	        @autosearch_buildings = Building.where('building_street_address @@ :q', q: params[:term])
-	        if @autosearch_buildings.present?
-	        	 @result_type = 'address'
-	        else
-	        	@autosearch_buildings = Building.where('zipcode @@ :q', q: params[:term])
-	        	@result_type = 'zipcode'
-	      	end
-	      end
-	    #end
+		@buildings = Building.text_search(params[:term])
+		# second search with building name
+  	@autosearch_buildings = Building.where('building_name @@ :q', q: params[:term])
+    if @autosearch_buildings.present?
+      @result_type = 'buildings'
+    else
+      # second search with city names
+      @autosearch_buildings = Building.where('city @@ :q', q: params[:term])
+      if @autosearch_buildings.present?
+        @result_type = 'cities'
+      else
+        # in last search with building address
+        @autosearch_buildings = Building.where('building_street_address @@ :q', q: params[:term])
+        if @autosearch_buildings.present?
+        	 @result_type = 'address'
+        else
+        	@autosearch_buildings = Building.where('zipcode @@ :q', q: params[:term])
+        	@result_type = 'zipcode'
+      	end
+      end
     end
-
-
+    # @polylines = []
+    # @buildings.each do |building|
+    #   @polylines << { 'lng': building.longitude, 'lat': building.latitude }
+    # end
+    # @polylines = [@polylines]
+      
   	if @buildings.present?
 	  	@hash = Gmaps4rails.build_markers(@buildings) do |building, marker|
 	      marker.lat building.latitude
--- app/views/home/search.json.jbuilder
+++ app/views/home/search.json.jbuilder
@@ -2,7 +2,7 @@ case @result_type
   when 'buildings'
     json.array! @buildings do |building|
       json.id building.id
-      json.search_term "#{building.building_name}, #{building.city}, #{building.state}"
+      json.search_term "#{building.building_name} - #{building.building_street_address}, #{building.city}, #{building.state}, #{building.zipcode}"
       json.term "#{building.building_name}"
     end
   when 'cities'
@@ -20,7 +20,7 @@ case @result_type
   when 'zipcode'
     json.array! @buildings do |building|
       json.id building.id
-      json.search_term "#{building.city}, #{building.state}, #{building.zipcode}"
+      json.search_term "#{building.zipcode} - #{building.city}, #{building.state}"
       json.term "#{building.zipcode}"
     end
 end
