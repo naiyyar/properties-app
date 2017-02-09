79067d4c1df964fb87157a665f532a6859513eac
Naiyyar Abbas <naiyyarabbas512013@gmail.com>
2017-02-09 11:25:32 +0530

    fix for zipcode search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,35 +4,34 @@ class HomeController < ApplicationController
 
   def search
     if params['apt-search-txt'].present?
-      @brooklyn_neighborhoods = params['apt-search-txt'].split(',')[0]
+      @brooklyn_neighborhoods = params['apt-search-txt'].split(',')[0] #used to add border boundaries of brooklyn and queens
       coordinates = Geocoder.coordinates(params['apt-search-txt'])
-      if params[:term].present?
-        building = Building.where(building_street_address: params[:term])
-        redirect_to building_path(building.first) if building.present?
-      elsif params[:neighborhoods].present?
-        @boundary_coords = []
-        @buildings = Building.buildings_in_neighborhood(params)
-        geo_coordinates = Gcoordinate.neighbohood_boundary_coordinates(params[:neighborhoods])
-        @boundary_coords << geo_coordinates
-        @zoom = 14
-      else
-        search = Geocoder.search(params['apt-search-txt'])
-        if search.present?
-           @boundary_coords = []
-          if search.first.types[0] == 'postal_code'
-            search_term = params['apt-search-txt'].split(' - ')
-            if(search_term.length > 1)
-              zipcode = search_term[0]
-              @buildings = Building.where('zipcode = ?',zipcode).paginate(:page => params[:page], :per_page => 10).to_a.uniq(&:building_street_address)
-            else
-              zipcode = params['apt-search-txt']
-            end
-            @boundary_coords << Gcoordinate.where(zipcode: zipcode).map{|rec| { lat: rec.latitude, lng: rec.longitude}}
+      search = Geocoder.search(params['apt-search-txt'])
+      @boundary_coords = []
+      
+      if search.present?
+        if search.first.types[0] == 'postal_code'
+          search_term = params['apt-search-txt'].split(' - ')
+          if(search_term.length > 1)
+            zipcode = search_term[0]
+            @buildings = Building.where('zipcode = ?',zipcode).paginate(:page => params[:page], :per_page => 10) #.to_a.uniq(&:building_street_address)
             @zoom = 14
           else
-            @boundary_coords << Gcoordinate.where(city: 'Manhattan').map{|rec| { lat: rec.latitude, lng: rec.longitude}}
-            @zoom = 12
+            zipcode = params['apt-search-txt']
           end
+          @boundary_coords << Gcoordinate.where(zipcode: zipcode).map{|rec| { lat: rec.latitude, lng: rec.longitude}}
+          @zoom = 14
+        elsif params[:term].present?
+          building = Building.where(building_street_address: params[:term])
+          redirect_to building_path(building.first) if building.present?
+        elsif params[:neighborhoods].present?
+          @buildings = Building.buildings_in_neighborhood(params)
+          geo_coordinates = Gcoordinate.neighbohood_boundary_coordinates(params[:neighborhoods])
+          @boundary_coords << geo_coordinates
+          @zoom = 14
+        else
+          @boundary_coords << Gcoordinate.where(city: 'Manhattan').map{|rec| { lat: rec.latitude, lng: rec.longitude}}
+          @zoom = 12
         end
       end
       if coordinates.present?
@@ -84,8 +83,8 @@ class HomeController < ApplicationController
 	  	@hash = Gmaps4rails.build_markers(@buildings) do |building, marker|
         marker.lat building.latitude
 	      marker.lng building.longitude
-	      building_link = view_context.link_to building.building_name, building_path(building)
-	      marker.title "#{building.id}, #{building.building_name}, #{building.building_street_address}, #{building.zipcode}"
+	      building_link = view_context.link_to building.building_name_or_address, building_path(building)
+	      marker.title "#{building.id}, #{building.building_name}, #{building.street_address}, #{building.zipcode}"
         
 	      marker.infowindow render_to_string(:partial => "/layouts/shared/marker_infowindow", :locals => { building_link: building_link, :building => building })
 	    end
