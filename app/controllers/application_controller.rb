2fe056e3286b859819d500d2123a104a8681db2c
Naiyyar Abbas <naiyyarabbas512013@gmail.com>
2017-02-15 10:53:01 +0530

    search for mobile landscape view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :allow_iframe_requests
+
   def after_sign_in_path_for(resource)
 	  if session[:form_data].present?
 	    reviewable = find_reviewable
@@ -38,6 +40,10 @@ class ApplicationController < ActionController::Base
 	end
 
 	private
+
+  def allow_iframe_requests
+    response.headers.delete('X-Frame-Options')
+  end
 	
 	def find_reviewable
     session[:form_data].each do |name, value|
