650c8e19268afeec1a06eb24d4ab04460c7c24d9
Naiyyar Abbas <naiyyarabbas512013@gmail.com>
2017-03-27 10:29:02 +0530

    fix for upload photo button

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -240,7 +240,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
-  config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_SECRET_ID'], callback_url: 'https://localhost:3000/users/auth/facebook/callback'
+  config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_SECRET_ID'], callback_url: '/users/auth/facebook/callback'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
