deed0af55ff63088c4670f9f141c9eeaf5ba6744
Naiyyar Abbas <naiyyarabbas512013@gmail.com>
2017-04-02 09:02:58 +0530

    fix for validation on start and end date

--- app/assets/javascripts/rental_price_histories.coffee
+++ app/assets/javascripts/rental_price_histories.coffee
@@ -1,3 +1,13 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+jQuery ->
+
+	$('#rental_price_history_residence_end_date').on 'changeDate',(e) ->
+		start_date = $("#rental_price_history_residence_start_date").val()
+		end_date = $("#rental_price_history_residence_end_date").val()
+		if((Date.parse(start_date) >= Date.parse(end_date)))
+			$('.validation_error_message').removeClass('hidden');
+		else
+			$('.validation_error_message').addClass('hidden');
--- app/models/rental_price_history.rb
+++ app/models/rental_price_history.rb
@@ -1,13 +1,13 @@
 class RentalPriceHistory < ActiveRecord::Base
 	belongs_to :unit
 
-	validate :residence_end_date_after_residence_start_date?
+	# validate :residence_end_date_after_residence_start_date?
 
-	def residence_end_date_after_residence_start_date?
-		if !residence_end_date.nil?
-		  if residence_end_date < residence_start_date
-		    errors.add :residence_end_date, 'Must be after resident from'
-		  end
-		end
-	end
+	# def residence_end_date_after_residence_start_date?
+	# 	if residence_end_date.present? and residence_start_date.present?
+	# 	  if residence_end_date < residence_start_date
+	# 	    errors.add :residence_end_date, 'Must be after resident from'
+	# 	  end
+	# 	end
+	# end
 end
