30bdc5fea756035be304fd6a6d91f53b9cf4b5ea
Naiyyar Abbas <naiyyarabbas512013@gmail.com>
2018-03-24 11:00:46 +0530

    show page neighborhood display changes

--- app/models/building.rb
+++ app/models/building.rb
@@ -73,6 +73,10 @@ class Building < ActiveRecord::Base
     end
   end
 
+  def neighbohoods
+    first_neighborhood.present? ? first_neighborhood : neighborhood3
+  end
+
   def image_uploads
     self.uploads.where('image_file_name is not null')
   end
