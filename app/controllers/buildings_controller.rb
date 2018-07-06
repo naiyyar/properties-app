320692568797e13b543d4decebd9fd466b81e564
naiyyarabbas <naiyyarabbas512013@gmail.com>
2018-07-06 11:42:15 +0530

    ipad header UI changes

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -21,6 +21,7 @@ class BuildingsController < ApplicationController
     session[:form_data] = nil if session[:form_data].present?
     @buildings = Building.text_search(params[:term]).reorder('building_street_address ASC').limit(10).includes(:units)
     @building = Building.where(id: params[:building_id]).first if params[:building_id].present?
+    @search_bar_hidden = :hidden
   end
 
   def import
