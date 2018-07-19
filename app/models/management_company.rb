07284f0365d9738338142ee91571ca0786f41737
naiyyarabbas <naiyyarabbas512013@gmail.com>
2018-07-19 15:25:32 +0530

    Management show page changes

--- app/models/management_company.rb
+++ app/models/management_company.rb
@@ -7,7 +7,26 @@
 class ManagementCompany < ActiveRecord::Base
 	has_many :buildings
 
+	#methods
+	
 	def add_building building_id
 		Building.where(id: building_id).update_all(management_company_id: self.id)
 	end
+
+	def aggregate_reviews
+		count = 0
+		self.buildings.each do |building|
+			count += building.reviews.count
+		end
+		count
+	end
+
+	# def aggregate_rating
+	# 	count = 0
+	# 	self.buildings.each do |building|
+	# 		count += building.reviews.count
+	# 	end
+	# 	count
+	# end
+
 end
