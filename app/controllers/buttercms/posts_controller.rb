1cbfa1b8e63563de5e59452c2848d5939d9b2520
naiyyarabbas <naiyyarabbas512013@gmail.com>
2018-08-18 10:55:30 +0530

    blog page UI changes

--- app/controllers/buttercms/posts_controller.rb
+++ app/controllers/buttercms/posts_controller.rb
@@ -1,15 +1,16 @@
 class Buttercms::PostsController < Buttercms::BaseController
   def index
     @posts = ButterCMS::Post.all(:page => params[:page], :page_size => 10)
-
+    @categories = ButterCMS::Category.all
     @next_page = @posts.meta.next_page
     @previous_page = @posts.meta.previous_page
   end
 
   def show
     @post = ButterCMS::Post.find(params[:slug])
-
+    @categories = @post.categories
     @next_post = @post.meta.next_post
     @previous_post = @post.meta.previous_post
   end
+
 end
--- config/initializers/buttercms.rb
+++ config/initializers/buttercms.rb
@@ -5,4 +5,4 @@ require 'buttercms-ruby'
 ButterCMS::api_token = ENV['BUTTER_TOKEN']
 
 # Test mode can be used to setup a staging website for previewing content or for testing content during local development.
-#ButterCMS::test_mode = true
+ButterCMS::test_mode = true
