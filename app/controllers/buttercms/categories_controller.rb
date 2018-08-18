3341d262ca0c8b33995cd197e256e2bd2d8a9df4
naiyyarabbas <naiyyarabbas512013@gmail.com>
2018-08-18 11:08:02 +0530

    category page UI changes

--- app/controllers/buttercms/categories_controller.rb
+++ app/controllers/buttercms/categories_controller.rb
@@ -1,5 +1,6 @@
 class Buttercms::CategoriesController < Buttercms::BaseController
   def show
     @category = ButterCMS::Category.find(params[:slug], :include => :recent_posts)
+    @categories = ButterCMS::Category.all
   end
 end
