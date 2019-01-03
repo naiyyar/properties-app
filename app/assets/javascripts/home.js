101961f344fc9e0e5d0d6d10e8f022d625c63dae
naiyyarabbas <naiyyarabbas512013@gmail.com>
2019-01-03 12:26:39 +0530

    Condition changed to check innerwidth on resize

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -467,22 +467,24 @@
     window.addEventListener('resize', function() {
         var homeSearchContainer = $('.home-search-form  .easy-autocomplete');
         var splitSearchContainer = $('.split-view-seach  .easy-autocomplete');
-        if(window.outerWidth > 500 && window.outerWidth <= 667){
-            homeSearchContainer.css('width','649px');
-            splitSearchContainer.css('width','581px');
-        }
-        else if(window.outerWidth > 667 && window.outerWidth <= 736){
-            homeSearchContainer.css('width','715px');
-            splitSearchContainer.css('width','646px');
-        }
-        else if(window.outerWidth == 375){
-            homeSearchContainer.css('width','355px');
-            splitSearchContainer.css('width','289px');
-        }
-        else if(window.outerWidth == 414){
-            homeSearchContainer.css('width','394px');
-            splitSearchContainer.css('width','322px');
-        }
+        setTimeout(function(){
+            if(window.innerWidth > 500 && window.innerWidth <= 667){
+                homeSearchContainer.css('width','649px');
+                splitSearchContainer.css('width','581px');
+            }
+            else if(window.innerWidth > 667 && window.innerWidth <= 736){
+                homeSearchContainer.css('width','715px');
+                splitSearchContainer.css('width','646px');
+            }
+            else if(window.innerWidth == 375){
+                homeSearchContainer.css('width','355px');
+                splitSearchContainer.css('width','289px');
+            }
+            else if(window.innerWidth == 414){
+                homeSearchContainer.css('width','394px');
+                splitSearchContainer.css('width','322px');
+            }
+        }, 200);
     }, false);
 
 })(jQuery);
