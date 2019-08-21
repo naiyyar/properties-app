1d33a32e7daa5eea363bd6611baaeee9aaae4f7a
naiyyarabbas <naiyyarabbas512013@gmail.com>
2019-08-21 15:27:23 +0530

    popular search amenities and neighborhood urls logic added

--- app/modules/popular_searches.rb
+++ app/modules/popular_searches.rb
@@ -1,33 +1,97 @@
 module PopularSearches
-
+	QUEENS_CITIES = ['Queens','Astoria','Corona','Flushing','Forest Hills','Kew Gardens','Long Island City','Rego Park']
+	
 	def buildings_by_popular_search params
-		st = params[:search_term]
-		case st
+		case params[:search_term]
 		when 'manhattan-apartments-for-rent'
-			Building.where(city: 'New York')
+			manhattan_buildings
 		when 'cheap-apartments-in-nyc'
-			Building.where(price: 1)
+			nyc_buildings.where(price: 1)
 		when 'studio-apartments-in-nyc'
-			Building.studio
+			nyc_buildings.studio
 		when 'affordable-apartments-for-rent-in-nyc'
-			Building.where(price: [1,2])
+			nyc_buildings.where(price: [1,2])
 		when 'one-bedroom-apartments-in-nyc'
-			Building.one_bed
+			nyc_buildings.one_bed
 		when 'cheap-studio-apartments-in-nyc'
-			Building.studio.where(price: [1,2])
+			nyc_buildings.studio.where(price: [1,2])
 		when 'luxury-apartments-in-manhattan'
-			Building.where(city: 'New York', price: [3,4])
+			manhattan_buildings.where(price: [3,4])
 		when '2-bedroom-apartments-in-nyc'
-			Building.two_bed
+			nyc_buildings.two_bed
 		when '3-bedroom-apartments-for-rent-in-nyc'
-			Building.three_bed
+			nyc_buildings.three_bed
 		when 'manhattan-apartments-for-rent-cheap'
-			Building.where(city: 'New York', price: [1,2])
+			manhattan_buildings.where(price: [1,2])
 		when 'affordable-luxury-apartments-in-nyc'
-			Building.where(price: [2,3])
+			nyc_buildings.where(price: [2,3])
+		#amenities
+		when 'doorman-buildings-in-nyc'
+			nyc_buildings.doorman
+		when 'pet-friendly-apartments-in-nyc'
+			nyc_buildings.where('pets_allowed_cats is true OR pets_allowed_dogs is true')
+		when 'nyc-apartments-with-pool'
+			nyc_buildings.swimming_pool
+		when 'walk-up-apartments-nyc'
+			nyc_buildings.walk_up
+		when 'affordable-doorman-buildings-nyc'
+			nyc_buildings.where(price: [1,2], doorman: true)
+		when 'nyc-apartments-with-gyms'
+			nyc_buildings.gym
+		#neighborhoods
+		when 'studio-apartments-in-brooklyn'
+			brooklyn_buildings.studio
+		when 'studios-for-rent-in-queens'
+			queens_buildings.studio
+		when '2-bedroom-apartments-in-brookly-for-rent'
+			brooklyn_buildings.two_bed
+		when '2-bedroom-apartments-in-queens-for-rent'
+			queens_buildings.two_bed
+		when 'upper-east-side-apartments-luxury'
+			buildings_in_neighborhood('Upper East Side')
+		when 'harlem-studio-apartments'
+			buildings_in_neighborhood('Harlem').studio
+		when 'long-island-city-studios'
+			buildings_in_neighborhood('Long Island City').studio
+		when 'upper-east-side-studio-apartments'
+			buildings_in_neighborhood('Upper East Side').studio
+		when 'upper-west-side-studio-apartments'
+			buildings_in_neighborhood('Upper West Side').studio
+		when "hell's-kitchen-studios"
+			buildings_in_neighborhood("Hell's Kitchen").studio
+		when 'west-village-studios'
+			buildings_in_neighborhood('West Village').studio
+		when '2-bedroom-apartments-upper-east-side'
+			buildings_in_neighborhood('Upper West Side').two_bed
+		when "hell's-kitchen-luxury-rentals"
+			buildings_in_neighborhood("Hell's Kitchen").where(price: [3,4])
+		when 'midtown-studio-apartments-nyc'
+			buildings_in_neighborhood('Midtown').studio
+		when 'midtown-east-luxury-rentals'
+			buildings_in_neighborhood('Midtown East').where(price: [3,4])
+		when 'upper-west-side-luxury-rental-buildings'
+			buildings_in_neighborhood('Upper West Side').where(price: [3,4])
+		when 'upper-east-side-apartments-for-rent-with-doorman'
+			buildings_in_neighborhood('Upper East Side').doorman
 		else
-			Building.all
+			nyc_buildings
 		end
+	end
+
+	def nyc_buildings
+		Building.all
+	end
+
+	def manhattan_buildings
+		Building.where(city: 'New York')
+	end
+
+	def brooklyn_buildings
+		Building.where(city: 'Brooklyn')
+	end
+
+	def queens_buildings
+		Building.where(city: QUEENS_CITIES)
 	end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,59 +12,5 @@
 # 	end
 # end
 
-##*** TODO LATER ***##
-# manhattan = [ 
-# 							'Manhattan Apartments For Rent', 'Cheap Apartments In NYC', 'Studio Apartments In NYC',
-# 							'Affordable Apartments For Rent In NYC', 'One Bedroom Apartments in NYC', 'Cheap Studio Apartments in NYC',
-# 							'Luxury Apartments in Manhattan', '2 Bedroom Apartments in NYC', '3 Bedroom Apartments For Rent in NYC',
-# 							'Manhattan Apartments For Rent Cheap', 'Affordable Luxury Apartments In NYC'
-# 						]
-
-
-# amenities = [	'Doorman Buildings In NYC', 'Pet Friendly Apartments In NYC', 'NYC Apartments With Pool', 
-# 							'Walk Up Apartments NYC', 'Affordable Doorman Buildings NYC', 'NYC Apartments With Gyms'
-# 						]
-
-
-# neighborhoods = [
-# 									'Studio Apartments in Brooklyn', 'Studios For Rent In Queens', '2 Bedroom Apartments in Brookly For Rent', 
-# 									'2 Bedroom Apartments in Queens For Rent', 'Upper East Side Apartments Luxury', 'Harlem Studio Apartments',
-# 									'Long Island City Studios', 'Upper East Side Studio Apartments', 'Upper West Side Studio Apartments',
-# 									"Hell's Kitchen Studios", 'West Village Studios', '2 Bedroom Apartments Upper East Side', "Hell's Kitchen Luxury Rentals",
-# 									'Midtown Studio Apartments NYC', 'Midtown East Luxury Rentals', 'Upper West Side Luxury Rental Buildings',
-# 									'Upper East Side Apartments For Rent With Doorman'
-# 								]
-
-# manhattan.each do |title|
-# 	slug = title.downcase.split(' ').join('-')
-# 	ps = PopularSearch.create(title: title, category: 'Manhattan', slug: slug)
-	
-# 	if title == 'Manhattan Apartments For Rent'
-# 		Building.where(city: 'New York').each do |Building|
-# 			PopularSearchBuilding.create(building_id: building_id, popular_search_id: ps.id)
-# 		end
-# 	elsif title == 'Cheap Apartments In NYC'
-# 	elsif title == 'Studio Apartments In NYC'
-# 	elsif title == 'Affordable Apartments For Rent In NYC'
-# 	elsif title == 'One Bedroom Apartments in NYC'
-# 	elsif title == 'Cheap Studio Apartments in NYC'
-# 	elsif title == 'Luxury Apartments in Manhattan'
-# 	elsif title == '2 Bedroom Apartments in NYC'
-# 	elsif title == '3 Bedroom Apartments For Rent in NYC'
-# 	elsif title == 'Manhattan Apartments For Rent Cheap'
-# 	elsif title == 'Affordable Luxury Apartments In NYC'
-# 	end
-# end
-
-# amenities.each do |title|
-# 	slug = title.downcase.split(' ').join('-')
-# 	PopularSearch.create(title: title, category: 'Amenities', slug: slug)
-# end
-
-# neighborhoods.each do |title|
-# 	slug = title.downcase.split(' ').join('-')
-# 	PopularSearch.create(title: title, category: 'Neighborhoods', slug: slug)
-# end
-
 
 
