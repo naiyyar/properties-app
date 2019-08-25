82af2c5fd61356c152b8a507c258acbc8e070007
naiyyarabbas <naiyyarabbas512013@gmail.com>
2019-08-25 09:47:41 +0530

    Changes for #1538 to 1541

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -3,7 +3,8 @@ module ListingsHelper
 		if rentals == 'past'
 			'<h4>Past Rental Listings</h4>'
 		else
-			"<h4 style='margin: 2px 0px;font-size: 14px;'> #{@building.building_name_or_address} </h2>" +
+			style = "style='margin: 2px 0px; font-size: #{browser.device.mobile? ? '14px' : '18px'};'"
+			"<h4 #{style}> #{@building.building_name_or_address} </h2>" +
 			"<p class='cardAddress text-muted font-sm'>#{@building.street_address} #{@building.zipcode}</p>" +
 			"<b style='font-size: 14px;'>#{@listings.count} Active Rental Listings</b>"
 		end
