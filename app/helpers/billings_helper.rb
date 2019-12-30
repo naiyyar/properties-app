8c725b448252fbd1b91f7587557416ed96faf7da
naiyyarabbas <naiyyarabbas512013@gmail.com>
2019-12-30 13:06:15 +0530

    fix for #1745

--- app/helpers/billings_helper.rb
+++ app/helpers/billings_helper.rb
@@ -2,4 +2,8 @@ module BillingsHelper
 	def brand_name brand=nil
 		brand.present? ? brand.downcase.split(' ').join('_') : 'generic'
 	end
+
+	def previous_page_path
+		params[:fb_id].present? ? edit_featured_building_path(params[:fb_id]) : session[:back_to]
+	end
 end
