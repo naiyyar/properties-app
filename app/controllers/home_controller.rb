ec8d7af762102c0e00369be3e995ae0f081b07dc
naiyyarabbas <naiyyarabbas512013@gmail.com>
2020-01-05 09:32:50 +0530

    fix for 1777

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -75,26 +75,26 @@ class HomeController < ApplicationController
     elsif results[:searched_by] == 'no-fee-management-companies-nyc'
       redirect_to management_company_path(results[:company].first) if results[:company].present?
     else
-      @buildings = @searched_buildings = results[:buildings]
+      @buildings       = @searched_buildings = results[:buildings]
       @boundary_coords = results[:boundary_coords] if results[:boundary_coords].present?
-      @zoom = results[:zoom] if results[:zoom].present?
-      @filters = results[:filters]
-      @tab_title_text = pop_search_tab_title if @filters.present?
+      @zoom            = results[:zoom]            if results[:zoom].present?
+      @filters         = results[:filters]
+      @tab_title_text  = pop_search_tab_title      if @filters.present?
     end
     
     if @buildings.present?
-      @buildings = @buildings.includes(:building_average, :featured_buildings)
-      page_num = params[:page].present? ? params[:page].to_i : 1
-      final_results = Building.with_featured_building(@buildings, page_num)
+      @buildings          = @buildings.includes(:building_average, :featured_buildings)
+      page_num            = params[:page].present? ? params[:page].to_i : 1
+      final_results       = Building.with_featured_building(@buildings, page_num)
       @per_page_buildings = final_results[1]
-      @all_buildings = final_results[0][:all_buildings] #with featured
-      @hash = final_results[0][:map_hash]
-      @lat = @hash[0]['latitude']
-      @lng = @hash[0]['longitude']
+      @all_buildings      = final_results[0][:all_buildings] #with featured
+      @hash               = final_results[0][:map_hash]
+      @lat                = @hash[0]['latitude']
+      @lng                = @hash[0]['longitude']
       #in meta_desc
-      building_ids = @buildings.pluck(:id)
-      @photos_count = Upload.cached_building_photos(building_ids).length rescue 0
-      @reviews_count = Review.where(reviewable_id: building_ids, reviewable_type: 'Building').count
+      building_ids        = @buildings.pluck(:id)
+      @photos_count       = Upload.cached_building_photos(building_ids).length rescue 0
+      @reviews_count      = Review.where(reviewable_id: building_ids, reviewable_type: 'Building').count
     else
       if @boundary_coords.present? and @boundary_coords.first.length > 1
         @lat = @boundary_coords.first.first[:lat]
