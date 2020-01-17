200725da8aef3c26c8554b6445739e466861800e
naiyyarabbas <naiyyarabbas512013@gmail.com>
2020-01-17 10:07:23 +0530

    asset host added to show images in email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,8 +97,8 @@ Rails.application.configure do
   }
 
   config.action_mailer.default_url_options = { host: 'https://aptreviews-app.herokuapp.com/' }
-  
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.asset_host          = ENV['SERVER_ROOT']
+  config.action_mailer.delivery_method     = :smtp
 
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
