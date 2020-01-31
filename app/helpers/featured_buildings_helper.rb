df0b9f92882c4ddd60ed0ab62e0947a87d385b3c
naiyyarabbas <naiyyarabbas512013@gmail.com>
2020-01-31 23:11:12 +0530

    manager tools page fix

--- app/helpers/featured_buildings_helper.rb
+++ app/helpers/featured_buildings_helper.rb
@@ -1,10 +1,10 @@
 module FeaturedBuildingsHelper
 
 	def start_date fb
-		Time.zone.parse(fb.try(:start_date).to_s).strftime('%Y-%m-%d %H:%M')
+		Time.zone.parse(fb.try(:start_date).to_s)&.strftime('%Y-%m-%d %H:%M')
 	end
 
 	def end_date fb
-		Time.zone.parse(fb.try(:end_date).to_s).strftime('%Y-%m-%d %H:%M')
+		Time.zone.parse(fb.try(:end_date).to_s)&.strftime('%Y-%m-%d %H:%M')
 	end
 end
