a0d246a7449edb71301307f851e06b29aca202f7
naiyyarabbas <naiyyarabbas512013@gmail.com>
2020-03-11 11:42:24 +0530

    fix for 2004, 2005

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -3,21 +3,21 @@ module ListingsHelper
 		if rentals == 'past'
 			'<h4>Past Rental Listings</h4>'
 		else
-			"<h4 #{address_styles}> #{@building.building_name_or_address} </h2>" +
-			"<p class='cardAddress text-muted font-sm'>#{@building.street_address} #{@building.zipcode}</p>" +
+			"<h4 #{name_styles}> #{@building.building_name_or_address} </h2>" +
+			"<p class='cardAddress text-muted address_font'>#{@building.street_address} #{@building.zipcode}</p>" +
 			"<b style='font-size: 13px;'>#{@listings.size} Active Rental Listings</b>"
 		end
 	end
 
-	def address_styles
-		"style='margin: 2px 0px; font-size: #{address_text_font};font-weight: #{address_text_weight}'"
+	def name_styles
+		"style='margin: 2px 0px; font-size: #{name_text_font};font-weight: #{address_text_weight}'"
 	end
 
 	def address_text_weight
-		browser.device.mobile? ? 'bolder' : ''
+		browser.device.mobile? ? 'bolder;' : ''
 	end
 
-	def address_text_font
-		browser.device.mobile? ? '14px' : '18px'
+	def name_text_font
+		browser.device.mobile? ? '14px;' : '18px;'
 	end
 end
