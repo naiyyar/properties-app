d2748324b3384347c348297c283c50682301033d
naiyyarabbas <naiyyarabbas512013@gmail.com>
2020-04-15 11:14:54 +0530

    split view preload key requests optimizations

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,5 +8,5 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w(home/main.css home/main.scss)
+Rails.application.config.assets.precompile += %w(home/main.css home/main.scss split_view/main.scss)
 Rails.application.config.assets.precompile += %w(*.js)
