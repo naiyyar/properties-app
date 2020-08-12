ced2ff3dc30bd0ad637d1522196d7e72fe141125
naiyyarabbas <naiyyarabbas512013@gmail.com>
2020-08-12 14:42:06 +0530

    fix for #2379

--- app/models/video_tour.rb
+++ app/models/video_tour.rb
@@ -25,15 +25,35 @@ class VideoTour < ApplicationRecord
 
 	default_scope { order('sort asc') }
 
-	def self.videos_by_categories videos, options={}
-		categories  = videos.pluck(:category)
-		category    = categories.include?('amenities') ? 'amenities' : categories.uniq.sort.first
-		video_tours = if options.present? && options[:limit] == 2
-                    videos.where(category: category).limit(options[:limit])
-                  else
-                    videos.where.not(category: nil)
-                  end
-    video_tours = video_tours.group_by{|v| v.category}
-    return video_tours, category
+	class << self
+		def categories videos
+			@categories ||= videos.pluck(:category).uniq.sort
+		end
+
+		def first_category videos
+			cats = categories(videos)
+			cats.include?('amenities') ? 'amenities' : cats.first
+		end
+
+		def second_category videos
+			cats = categories(videos)
+			cats.include?('studio') ? 'studio' : cats[1]
+		end
+
+		def videos_by_categories videos, options={}
+			video_tours = unless options.empty?
+	                    first_2_videos(videos)
+	                  else
+	                    videos.where.not(category: nil)
+	                  end
+	    video_tours = video_tours.group_by{|v| v.category}
+	    return video_tours, first_category(videos)
+		end
+
+		def first_2_videos videos
+			tour_videos = videos.where(category: first_category(videos)).limit(2)
+	    return tour_videos if tour_videos.length >= 2
+	    videos.where(category: [first_category(videos), second_category(videos)]).limit(2)
+		end
 	end
 end
