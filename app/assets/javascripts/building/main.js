a08d7181aa66f2f8c914a1d76f166fa6d7f4c1aa
naiyyarabbas <naiyyarabbas512013@gmail.com>
2020-09-23 12:28:50 +0530

    changes for #2431-Mobile: Page Speed Insights Web Vitals For Building Show Page

--- app/assets/javascripts/building/main.js
+++ app/assets/javascripts/building/main.js
@@ -5,9 +5,7 @@
 //= require jquery.slimscroll.min
 //= require jquery.search
 //= require jquery.fancybox
-//= require filters
 //= require gmap
-//= require back_to_scroll_position
 //= require infobubble
 //= require swiper.jquery
 //= require dropdown_buttons
