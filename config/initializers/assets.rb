ef6a67b14f9b137584ce841afec3a9581aec4b98
naiyyarabbas <naiyyarabbas512013@gmail.com>
2020-10-03 13:50:49 +0530

    Show page unused css removed

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,5 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(home/main.css home/main.scss split_view/main.scss)
-Rails.application.config.assets.precompile += %w(building/main.css building/main.scss building/show.scss)
+Rails.application.config.assets.precompile += %w(building/main.css building/main.scss building/show.scss reviews.css)
 Rails.application.config.assets.precompile += %w(*.js)
