e75f6c6544c384b4cc64c740589807eb37971300
naiyyarabbas <naiyyarabbas512013@gmail.com>
2020-10-09 10:16:56 +0530

    setting zone script moved to different js file

--- app/assets/javascripts/home/main.js
+++ app/assets/javascripts/home/main.js
@@ -182,16 +182,3 @@ $('.dropdown-toggle-neighborhoods, .closeHoods').click(function(e) {
   });
 });
 
-
-// setting timezone
-$('.user_time_zone').set_timezone(); 
-const timezone = $('.user_time_zone').val(); 
-// setting in cookies to access using helper method on application controller
-function setCookie(name, value) {
-  var expires = new Date()
-  expires.setTime(expires.getTime() + (24 * 60 * 60 * 1000))
-  document.cookie = name + '=' + value + ';expires=' + expires.toUTCString()
-}
-
-setCookie("timezone", timezone)
-
---
+++ app/assets/javascripts/set_timezone.js
@@ -0,0 +1,16 @@
+var ready = function(){
+	// setting timezone
+	$('.user_time_zone').set_timezone(); 
+	const timezone = $('.user_time_zone').val(); 
+	// setting in cookies to access using helper method on application controller
+	function setCookie(name, value) {
+	  var expires = new Date()
+	  expires.setTime(expires.getTime() + (24 * 60 * 60 * 1000))
+	  document.cookie = name + '=' + value + ';expires=' + expires.toUTCString()
+	}
+
+	setCookie("timezone", timezone);
+}
+
+$(document).ready(ready);
+$(document).on("page:load", ready);
