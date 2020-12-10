ba7628903f1a5eebb5c88c9d8295fce6c13e8136
naiyyarabbas <naiyyarabbas512013@gmail.com>
2020-12-10 17:03:56 +0530

    changes for #2503-Split View Pages

--- app/helpers/uploads_helper.rb
+++ app/helpers/uploads_helper.rb
@@ -1,9 +1,9 @@
 module UploadsHelper
 
-	def seel_all_link building_id, images_count
+	def seel_all_link building_id, images_count, see_all_class=nil
 		link_to "<span style='color: #333;' class='fa fa-th-large'></span> See all(#{images_count})".html_safe, 
 						building_uploads_path(building_id: building_id), 
-						class: 'btn btn-o btn-sm see-all'
+						class: "btn btn-o btn-sm #{see_all_class}"
 	end
 
 	def object_upload_path object
@@ -17,8 +17,6 @@ module UploadsHelper
 	def associated_object imageable
 		if imageable.class.name == 'Unit'
 			imageable.name
-		else
-			#imageable.building_name
 		end
 	end
 
