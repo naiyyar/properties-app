0ada32dd6839d3f0e166f02fbf8b97bb8a8dd7df
naiyyarabbas <naiyyarabbas512013@gmail.com>
2020-12-10 13:28:00 +0530

    desktop view search clear text icon fix

--- app/assets/javascripts/search_modal.js
+++ app/assets/javascripts/search_modal.js
@@ -1,48 +1,50 @@
 var ready = function(){
-	var search_box 		= $('#search_term');
-	var search_modal 	= $('#search-modal');
-	var search_form 	= $('#apt-search-form');
-	var clear_icon 		= $('#clear-icon');
+	if(mobile){
+		var search_box 		= $('#search_term');
+		var search_modal 	= $('#search-modal');
+		var search_form 	= $('#apt-search-form');
+		var clear_icon 		= $('#clear-icon');
 
-	$('#search-input-placeholder').on('click', function(){
-    search_modal.css('z-index', '99999').show();
-    search_box[0].focus();
-    clearSearchField();
-	});
+		$('#search-input-placeholder').on('click', function(){
+	    search_modal.css('z-index', '99999').show();
+	    search_box[0].focus();
+	    clearSearchField();
+		});
 
-	$('.fa-arrow-left').on('click', function(){
-		hideClearIcon();
-	  search_modal.hide();
-	});
-
-	search_box.on('keyup', function(){
-		if($(this).val() != ''){			
-			showClearIcon();
-		}else{
+		$('.fa-arrow-left').on('click', function(){
 			hideClearIcon();
+		  search_modal.hide();
+		});
+
+		search_box.on('keyup', function(){
+			if($(this).val() != ''){			
+				showClearIcon();
+			}else{
+				hideClearIcon();
+			}
+		});
+		
+		clear_icon.on('click', function(){
+			clearSearchField();
+		});
+
+		function clearSearchField(){
+			$(search_box).val('');
+			setTimeout(function(){
+				hideClearIcon();
+			}, 400)
 		}
-	});
-	
-	clear_icon.on('click', function(){
-		clearSearchField();
-	});
 
-	function clearSearchField(){
-		$(search_box).val('');
-		setTimeout(function(){
-			hideClearIcon();
-		}, 400)
-	}
-
-	function showClearIcon(){
-		if(clear_icon.is(':hidden')){
-			clear_icon.show();
+		function showClearIcon(){
+			if(clear_icon.is(':hidden')){
+				clear_icon.show();
+			}
 		}
-	}
 
-	function hideClearIcon(){
-		if(!clear_icon.is(':hidden')){
-			clear_icon.hide();
+		function hideClearIcon(){
+			if(!clear_icon.is(':hidden')){
+				clear_icon.hide();
+			}
 		}
 	}
 
