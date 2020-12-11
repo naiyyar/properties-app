4026dbc576418bdf57b3a8949227ef533a2e49fc
naiyyarabbas <naiyyarabbas512013@gmail.com>
2020-12-11 16:15:59 +0530

    mobile current location rendering fix

--- app/assets/javascripts/location.js
+++ app/assets/javascripts/location.js
@@ -1,27 +1,28 @@
 var location_url = 'javascript:void(0);';
 function getLocation(){
   document.getElementById('search_term').value = 'Current Location';
-  navigator.permissions && navigator.permissions.query({name: 'geolocation'}).then(function(PermissionStatus) {
-    if(PermissionStatus.state == 'granted'){
+  //navigator.permissions && navigator.permissions.query({name: 'geolocation'}).then(function(PermissionStatus) {
+    //if(PermissionStatus.state == 'granted'){
       if (navigator.geolocation) {
         var options = {enableHighAccuracy: true, timeout: 60000, maximumAge: 0};
         navigator.geolocation.getCurrentPosition(showPosition, showError, options);
       } else {
         alert("Geolocation is not supported by this browser.");
       }
-    }
-    else{
-      console.log("Permission not granted. status_type:"+PermissionStatus.state);
-    }
-  });
+    //}
+    //else{
+    //  alert("Permission not granted. state_type:"+PermissionStatus.state);
+    //}
+  //});
 }
 
 function showPosition(position) {
   var loc_link = $('.ui-autocomplete li.curr-location a');
+  var url_to_render = render_url(position);
   if(loc_link.attr('href') == 'javascript:void(0);'){
-    window.location.href = render_url(position);
+    setTimeout(function(){document.location.href = url_to_render}, 250);
   }
-  loc_link.attr('href', render_url(position));
+  loc_link.attr('href', url_to_render);
 }
 
 function render_url(position){
@@ -37,10 +38,10 @@ function showError(error) {
       alert("Location information is unavailable.");
       break;
     case error.TIMEOUT:
-      console.log("The request to get user location timed out.");
+      alert("The request to get user location timed out.");
       break;
     case error.UNKNOWN_ERROR:
-      console.log("An unknown error occurred.");
+      alert("An unknown error occurred.");
       break;
   }
 }
