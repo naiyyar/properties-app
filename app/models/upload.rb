b7fa8a1011ec0f2fa51bc90f18cc595d2ded14cd
naiyyarabbas <naiyyarabbas512013@gmail.com>
2021-01-14 21:09:30 +0530

    upload images callback code optimizations

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,65 +1,44 @@
-# == Schema Information
-#
-# Table name: uploads
-#
-#  id                    :integer          not null, primary key
-#  image_file_name       :string
-#  image_content_type    :string
-#  image_file_size       :integer
-#  image_updated_at      :datetime
-#  imageable_id          :integer
-#  imageable_type        :string
-#  created_at            :datetime         not null
-#  updated_at            :datetime         not null
-#  user_id               :integer
-#  sort                  :integer
-#  rotation              :integer          default(0), not null
-#  document_file_name    :string
-#  document_content_type :string
-#  document_file_size    :integer
-#  document_updated_at   :datetime
-#  file_uid              :string
-#
-
 class Upload < ApplicationRecord
+  resourcify
+  
   IMG_CONTENT_TYPES = ['image/jpeg', 'image/gif', 'image/png']
 	attr_accessor :rotation_degrees, :rotate
-	resourcify
-	belongs_to :imageable, polymorphic: true
+	
+  # associations
+  belongs_to :imageable, polymorphic: true
   has_many :document_downloads
   belongs_to :user
-  
-  default_scope { order('sort asc') }
 
-  scope :with_image, -> { where.not(image_file_name: nil).includes(:imageable) }
-  scope :with_doc,   -> { where.not(document_file_name: nil) }
-
-	has_attached_file :image, 
+  # validations
+  has_attached_file :image, 
                     :styles => { :small => '200x200', :original => '900x800', :medium => '550x450' },
                     :default_url => "/images/:style/missing.png",
                     #:convert_options => {:medium => '-quality 80 -strip' },
                     processors: [:rotator]
-	
-	validates_attachment :image, 
-                				# :presence => true,
-                				:content_type => { :content_type => IMG_CONTENT_TYPES }
+  
+  validates_attachment :image,
+                        :content_type => { :content_type => IMG_CONTENT_TYPES }
   
   has_attached_file :document
   validates_attachment  :document, 
-                        # :presence => true,
                         :content_type => { :content_type => ['application/pdf','application/vnd.ms-excel',     
                                                              'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
                                                              'application/msword', 
                                                              'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 
                                                              'text/plain'] 
                                           }
+  
+  # scopes
+  default_scope { order('sort asc') }
+  scope :with_image, -> { where.not(image_file_name: nil).includes(:imageable) }
+  scope :with_doc,   -> { where.not(document_file_name: nil) }
+
+  # callbacks
   after_create :set_sort_index
   after_save :update_counter_cache
   after_destroy :update_counter_cache
 
-  def self.uploads_json_hash(uploads)
-    uploads.as_json(:methods => [:date_uploaded, :orig_image_url])
-  end
+  # Methods
 
   def date_uploaded
     created_at.strftime("%m/%d/%Y")
@@ -67,15 +46,6 @@ class Upload < ApplicationRecord
 
   def orig_image_url
     image.url
-  end
-
-  def self.building_photos building_ids
-    where(imageable_id: building_ids, imageable_type: 'Building')
-  end
-
-  def self.cached_building_photos building_ids
-    key = building_ids.present? ? building_ids.join('_') : '1'
-    Rails.cache.fetch([self, 'building_photos', key]) { building_photos(building_ids) }
   end
 
   def small_img
@@ -96,25 +66,11 @@ class Upload < ApplicationRecord
     uploaded_img_url(style)
   end
 
-  def self.image_uploads_count object
-    object.uploads.where('image_file_name is not null').count
-  end
-
-	def self.marker_image object
-    no_image = 'no-photo.jpg'
-    uploads  = object.image_uploads
-    return no_image if uploads.blank?
-    image_obj = uploads.first
-    if image_obj.present?
-      image_obj.image_file_name.present? ? image_obj.uploaded_img_url : no_image
-    end
-	end
-
   def total_downloads
     self.document_downloads.count
   end
 
-	def rotate!(degrees = 90)
+  def rotate!(degrees = 90)
     self.rotation += degrees
     self.rotation -= 360 if self.rotation >= 360
     self.rotation += 360 if self.rotation <= -360
@@ -133,33 +89,65 @@ class Upload < ApplicationRecord
     self.image_file_name != nil
   end
 
+  # class Methods
+  ##################################
+
+  def self.building_photos building_ids
+    where(imageable_id: building_ids, imageable_type: 'Building')
+  end
+
+  def self.cached_building_photos building_ids
+    key = building_ids.present? ? building_ids.join('_') : '1'
+    Rails.cache.fetch([self, 'building_photos', key]) { building_photos(building_ids) }
+  end
+
+  def self.uploads_json_hash(uploads)
+    uploads.as_json(:methods => [:date_uploaded, :orig_image_url])
+  end
+  
+  def self.image_uploads_count object
+    object.uploads.where('image_file_name is not null').count
+  end
+
+	def self.marker_image object
+    no_image = 'no-photo.jpg'
+    uploads  = object.image_uploads
+    return no_image if uploads.blank?
+    image_obj = uploads.first
+    if image_obj.present?
+      image_obj.image_file_name.present? ? image_obj.uploaded_img_url : no_image
+    end
+	end
+
   private
+  
   def set_defaults
     self.rotation ||= 0
   end
 
   def set_sort_index
-    imageable = self.imageable
-    uploads = Upload.where(imageable_id: imageable.id, imageable_type: 'Building')
+    uploads = Upload.where(imageable_id: self.imageable.id, imageable_type: 'Building')
                     .includes(:imageable).order('sort ASC NULLS LAST, created_at ASC')
     uploads.each_with_index do |upload, index|
-      upload.update(sort: index+1) 
+      upload.update_column('sort', index+1) 
     end
   end
 
   def update_counter_cache
     begin
-      uploads = Upload.where('image_file_name is not null AND 
-                              imageable_id = ? AND 
-                              imageable_type = ?', imageable.id, imageable.class.name)
-      self.imageable.uploads_count = uploads.count
-      self.imageable.save
+      self.imageable.update_column('uploads_count', uploads_count)
     rescue Exception => e
       puts "SQL error in #{ __method__ }"
       ActiveRecord::Base.connection.execute 'ROLLBACK'
 
       raise e
     end
+  end
+
+  def uploads_count
+    Upload.where('image_file_name is not null AND 
+                  imageable_id = ? AND 
+                  imageable_type = ?', imageable.id, imageable.class.name).size
   end
 
   # def is_video?
