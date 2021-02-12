9237f9bfabb0a05ddb18cc9e90c88d8bdb45f35b
naiyyarabbas <naiyyarabbas512013@gmail.com>
2021-02-13 00:30:00 +0530

    show page image slider fix

--- app/models/concerns/imageable_concern.rb
+++ app/models/concerns/imageable_concern.rb
@@ -1,14 +1,15 @@
 module ImageableConcern
 	extend ActiveSupport::Concern
-	included do
+	
+  included do
 		has_many :uploads, as: :imageable, dependent: :destroy
 		accepts_nested_attributes_for :uploads, :allow_destroy => true
 	end
 
   def get_uploads
     uploads_hash = Hash.new
-    assets       = self.uploads.all
-    uploads_hash.merge!({ image_uploads: chached_image_uploads(assets), 
+    assets       = self.uploads
+    uploads_hash.merge!({ image_uploads: cached_image_uploads(assets), 
                          doc_uploads:   doc_uploads(assets) })
   end
 
@@ -17,8 +18,8 @@ module ImageableConcern
     assets.with_image
   end
 
-  def chached_image_uploads assets
-    Rails.cache.fetch([self, 'image_uploads']) { image_uploads(assets).to_a }
+  def cached_image_uploads assets
+    Rails.cache.fetch([self, 'imageUploads']) { image_uploads(assets).to_a }
   end
 
   def doc_uploads assets
