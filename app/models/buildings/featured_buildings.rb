144c7b153560e63428f204ed1fbdd6267bcc0a96
naiyyarabbas <naiyyarabbas512013@gmail.com>
2021-03-01 11:26:29 +0530

    split view adding featured properties / agent module code refactoring

--- app/models/buildings/featured_buildings.rb
+++ app/models/buildings/featured_buildings.rb
@@ -8,27 +8,27 @@ module Buildings
 	    @buildings = buildings
 	    @featured_listings = featured_listings
 	    @agent = agent
-	    buildings = top2_featured + non_featured if top2_featured.present?
-	    # when there are only top two featured buildings after filter
-	    # then per_page_buildings will be blank due to fitering featured buildings
-	    if per_page_buildings.blank? && all_buildings.present?
-	      per_page_buildings = all_buildings.paginate(:page => page_num, :per_page => PER_PAGE)
-	    end
+	    @buildings = top2_featured + non_featured if top2_featured.present?
+	    @per_page_buildings = non_featured.paginate(:page => page_num, :per_page => PER_PAGE)
+	    @per_page_buildings = paginate_all_buildings if @per_page_buildings.blank? && all_buildings.present?
+	    
 	    final_results.merge!({
 	    	all_buildings: with_featured_listings_and_agent,
-	    	map_hash: Building.buildings_json_hash(buildings)
+	    	map_hash: Building.buildings_json_hash(@buildings)
 	    })
 	    
-	    return final_results[:all_buildings], final_results[:map_hash], per_page_buildings
+	    return final_results[:all_buildings], final_results[:map_hash], @per_page_buildings
 	  end
 
 	  private
-
-	  def with_featured_listings_and_agent
-	  	buildings_with_featured_agent
+	  
+	  # when there are only top two featured buildings after filter
+	  # then per_page_buildings will be blank due to fitering featured buildings
+	  def paginate_all_buildings
+	  	all_buildings.paginate(:page => page_num, :per_page => PER_PAGE)
 	  end
 
-	  def buildings_with_featured_agent
+	  def with_featured_listings_and_agent
 	  	return buildings_with_featured_listings if @agent.blank?
 	  	insert_at_index = total_featured_property + (FEATURED_AGENT_THUMBNAIL_POS - total_featured_property)
 	  	
@@ -48,19 +48,15 @@ module Buildings
 	  end
 
 	  def all_buildings 
-	  	@all_buildings ||= buildings_with_featured_on_top(top2_featured, per_page_buildings)
-	  end
-
-	  def per_page_buildings
-	  	@per_page_buildings ||= non_featured.paginate(:page => page_num, :per_page => PER_PAGE)
+	  	@all_buildings ||= buildings_with_featured_on_top
 	  end
 
 	  def non_featured
-	  	@non_featured ||= non_featured_buildings(@buildings, top2_featured)
+	  	@non_featured ||= non_featured_buildings
 	  end
 
 	  def top2_featured
-	  	@top2_featured ||= top2_featured_buildings(@buildings)
+	  	@top2_featured ||= top2_featured_buildings
 	  end
 
 	  def featured_buildings_count
@@ -68,19 +64,19 @@ module Buildings
 	  end
 	  
 	  # putting featured building on top
-	  def buildings_with_featured_on_top top_2, per_page
-	    top_2.present? ? (top_2 + per_page) : per_page
+	  def buildings_with_featured_on_top
+	    top2_featured.present? ? (top2_featured + @per_page_buildings) : @per_page_buildings
 	  end
 
-	  def non_featured_buildings buildings, top_2
-	    buildings = buildings.where.not(id: top_2.map(&:id)) if top_2.present?
+	  def non_featured_buildings
+	    buildings = @buildings.where.not(id: top2_featured.map(&:id)) if top2_featured.present?
 	    return Building.sort_buildings(buildings, @sort_by, @filters_params)
 	  end
 
-	  def top2_featured_buildings buildings
-	    fb_ids = featured_building_ids(buildings.pluck(:id))
+	  def top2_featured_buildings
+	    fb_ids = featured_building_ids(@buildings.pluck(:id))
 	    return [] if fb_ids.blank?
-	    buildings.random(fb_ids.shuffle[0..1])
+	    @buildings.random(fb_ids.shuffle[0..1])
 	  end
 
 	  def featured_building_ids ids
