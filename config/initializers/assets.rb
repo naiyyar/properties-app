a10f4fe74fb770e4d4c6da2c42c52db985c218d7
naiyyarabbas <naiyyarabbas512013@gmail.com>
2021-06-25 13:21:37 +0530

    Home view css files chaining removed

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,6 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-Rails.application.config.assets.precompile += %w(home/main.css home/main.scss search-view.css)
+Rails.application.config.assets.precompile += %w(home-main.css search-view.css)
 Rails.application.config.assets.precompile += %w(building/main.css building/main.scss building/show.scss reviews.css)
 Rails.application.config.assets.precompile += %w(*.js)
